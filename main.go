b4e8614e2bb75cb63dfe8df71ea3fb30dae55b96
Diego López Torres <buyeye09@gmail.com>
2022-05-06 18:43:51 -0500

    successful queries

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/joho/godotenv"
 )
+
+type Store struct {
+	Name string
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -28,4 +34,34 @@ func main() {
 	if err != nil {
 		log.Fatalln("ping failed:", err.Error())
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	query := `
+		SELECT s.store_name
+			FROM
+				BikeStores.sales.order_items oi
+		INNER JOIN
+			BikeStores.sales.orders o ON o.order_id =  oi.product_id
+		INNER JOIN
+			BikeStores.sales.stores s ON o.store_id = s.store_id;
+	`
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		log.Fatalln("error at querying database:", err.Error())
+	}
+
+	stores := []Store{}
+	for rows.Next() {
+		store := Store{}
+		err := rows.Scan(&store.Name)
+		if err != nil {
+			log.Fatalln("error while scanning rows", err.Error())
+		}
+		stores = append(stores, store)
+	}
+
+	fmt.Println("number of rows", len(stores))
 }
