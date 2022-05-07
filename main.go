778a1a2a382b643848136513b43212b396cb4d0e
Diego López Torres <buyeye09@gmail.com>
2022-05-06 19:53:01 -0500

    finished

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -21,20 +22,48 @@ func main() {
 		log.Fatalln("could not load environment variables:", err.Error())
 	}
 
-	dsn := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;",
-		os.Getenv("DB_SERVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("PORT"))
+	connectionUri := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;",
+					os.Getenv("DB_SERVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("PORT"))
 
-	db, err := sql.Open("sqlserver", dsn)
+	db, err := ConnectToDatabase(connectionUri)
 	if err != nil {
-		log.Fatalln("could not connect to database", err.Error())
+		log.Fatalln(err.Error())
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	stores, err := QueryStores(db)
 	if err != nil {
-		log.Fatalln("ping failed:", err.Error())
+		log.Fatalln(err.Error())
 	}
 
+	fmt.Println("\t¿En qué tienda será la sig. venta con base en las ordenes pasadas?")
+	fmt.Println(">>Ejecutando algoritmo ZeroR...")
+
+	rule := GenerateZeroRRule(stores)
+
+	fmt.Println("\tLa tienda a predecir será", rule)
+	fmt.Println(">>Calculando precisión de la predicción...")
+
+	accuracy := CalculatePredictionAccuracy(stores, rule)
+
+	fmt.Printf("\tLa precisión de las predicciones fue de un %.2f por ciento.\n", accuracy * 100)
+}
+
+func ConnectToDatabase(connectionUri string) (*sql.DB, error) {
+	db, err := sql.Open("sqlserver", connectionUri)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("invalid uri: %s", err.Error()))
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("ping failed: %s", err.Error()))
+	}
+
+	return db, nil
+}
+
+func QueryStores(db *sql.DB) ([]Store, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
@@ -42,15 +71,15 @@ func main() {
 		SELECT s.store_name
 			FROM
 				BikeStores.sales.order_items oi
-		INNER JOIN
-			BikeStores.sales.orders o ON o.order_id =  oi.product_id
-		INNER JOIN
-			BikeStores.sales.stores s ON o.store_id = s.store_id;
+			INNER JOIN
+				BikeStores.sales.orders o ON o.order_id =  oi.product_id
+			INNER JOIN
+				BikeStores.sales.stores s ON o.store_id = s.store_id;
 	`
 
 	rows, err := db.QueryContext(ctx, query)
 	if err != nil {
-		log.Fatalln("error at querying database:", err.Error())
+		return nil, errors.New(fmt.Sprintf("error at querying database: %s", err.Error()))
 	}
 
 	stores := []Store{}
@@ -58,10 +87,47 @@ func main() {
 		store := Store{}
 		err := rows.Scan(&store.Name)
 		if err != nil {
-			log.Fatalln("error while scanning rows", err.Error())
+			return nil, errors.New(fmt.Sprintf("error while scanning rows: %s", err.Error()))
 		}
 		stores = append(stores, store)
 	}
 
-	fmt.Println("number of rows", len(stores))
+	if err := rows.Err(); err != nil {
+		return nil, errors.New(fmt.Sprintf("error at scanning: %s", err.Error()))
+	}
+
+	return stores, nil
+}
+
+func GenerateZeroRRule(stores []Store) string {
+	storeRepetitions := make(map[string]int)
+	trainingSetLength := len(stores) / 3
+
+	for _, store := range stores[:trainingSetLength - 1] {
+		storeRepetitions[store.Name]++
+	}
+
+	max, selectedStore := 0, ""
+	for store, orders := range storeRepetitions {
+		if orders > max {
+			selectedStore = store
+		}
+	}
+
+	return selectedStore
+}
+
+func CalculatePredictionAccuracy(stores []Store, selectedStore string) float32 {
+	trainingSetLength := len(stores) / 3
+	testSetLength := len(stores) - trainingSetLength
+	asserts := 0
+	
+	for _, store := range stores[trainingSetLength - 1:] {
+		if store.Name == selectedStore {
+			asserts++
+		}
+	}
+
+	accuracy := float32(asserts) / float32(testSetLength)
+	return accuracy
 }
